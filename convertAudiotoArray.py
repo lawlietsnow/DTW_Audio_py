8d036874dd94c5336da7b1b433542a65ddd52bb0
nvthangmonkey <94818791+nvthangmonkey@users.noreply.github.com>
2022-04-08 14:58:23 +0700

    commit

---
+++ convertAudiotoArray.py
@@ -0,0 +1,15 @@
+import wave
+import numpy 
+                                                                                             
+ifile = wave.open("E:/DTW_Audio_py/mjRecord2.wav")
+samples = ifile.getnframes()
+audio = ifile.readframes(samples)
+
+# Convert buffer to float32 using NumPy                                                                                 
+audio_as_np_int16 = numpy.frombuffer(audio, dtype=numpy.int16)
+audio_as_np_float32 = audio_as_np_int16.astype(numpy.float32)
+                                                     
+max_int16 = 2**15
+audio_normalised = audio_as_np_float32 / max_int16
+for i in audio_normalised:
+    print (i)
